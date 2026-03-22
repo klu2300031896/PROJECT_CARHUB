f8aad3d1cb0a5a2d8648beb76e1a4ce82b2f5d24
klu2300031896 <2300031896@kluniversity.in>
2026-03-22 15:10:04 +0530

    Update api.js

--- carhub-frontend/src/services/api.js
+++ carhub-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://carhub-backend-production-39e4.up.railway.app/api"
+  baseURL: "https://carhub-backend-production-39e4.up.railway.app/api"
 });
 
 export default API;
